b71a2f62dd64ca30da2a281a811f39741a8548cc
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2026-04-09 23:21:04 -0400

    Removed local helper + switched to shared helpers
    Switched all of the entity/tile creation in Map1Factory.cs to use the
    shared helper methods I created in MapEntityFactor.cs
    
    Also removed the local tile helper, as that has been extracted to the
    shared helper class

--- src/Group Project/Maps/Map1Factory.cs
+++ src/Group Project/Maps/Map1Factory.cs
@@ -8,68 +8,58 @@ namespace Group_Project {
 
     public static class MapFactory {
 
-        // helper method for building tiles
-        private static Tile MakeTile(int row, int col, TileType type, string entityName = null,
-            string imagePath = null, int destination = -1) {
-                Tile tile = new Tile(row, col, type);
-                tile.EntityName = entityName;
-                tile.ImagePath = imagePath;
-                tile.DestinationSectionIndex = destination;
-                return tile;
-        }
-
         public static GameMap BuildMap1() {
 
             // create map and populate with enemies, NPCs, items, and sections
             GameMap map1 = new GameMap("Map 1");
 
             // [ENEMY] Regenerator
-            Enemy regenerator = new Enemy("Regenerator", 75, 10, 20, false);
-            // pathing for assets
-            regenerator.SpriteIconPath =
-            @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_SpriteIcon.png";
-            regenerator.InteractionIconPath =
-            @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_InteractionIcon.png";
-            regenerator.DeathSoundPath =
-            @"assets\sounds\npcs\Hostiles\Regenerator\Regenerator_EnemyDeath.wav";
-            regenerator.CriticalHitSoundPath =
-            @"assets\sounds\npcs\Hostiles\Regenerator\Regenerator_EnemyCriticalHit.wav";
+            Enemy regenerator = MapEntityFactory.CreateEnemy(
+                "Regenerator",
+                75,
+                10,
+                20,
+                false,
+                @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_SpriteIcon.png",
+                @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_InteractionIcon.png",
+                @"assets\sounds\npcs\Hostiles\Regenerator\Regenerator_EnemyDeath.wav",
+                @"assets\sounds\npcs\Hostiles\Regenerator\Regenerator_EnemyCriticalHit.wav");
 
             // [ENEMY] Dr. Salvador
-            Enemy drSalvador = new Enemy("DrSalvador", 100, 30, 30, false);
-            // pathing for assets
-            drSalvador.SpriteIconPath =
-            @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_SpriteIcon.jpg";
-            drSalvador.InteractionIconPath =
-            @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_InteractionIcon.png";
-            drSalvador.DeathSoundPath =
-            @"assets\sounds\npcs\Hostiles\DrSalvador\DrSalvador_EnemyDeath.wav";
-            drSalvador.CriticalHitSoundPath =
-            @"assets\sounds\npcs\Hostiles\DrSalvador\DrSalvador_EnemyCriticalHit.wav";
+            Enemy drSalvador = MapEntityFactory.CreateEnemy(
+                "DrSalvador",
+                100,
+                30,
+                30,
+                false,
+                @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_SpriteIcon.jpg",
+                @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_InteractionIcon.png",
+                @"assets\sounds\npcs\Hostiles\DrSalvador\DrSalvador_EnemyDeath.wav",
+                @"assets\sounds\npcs\Hostiles\DrSalvador\DrSalvador_EnemyCriticalHit.wav");
 
             // [ENEMY] BigDaddy
-            Enemy bigDaddy = new Enemy("BigDaddy", 200, 7.5, 30, false);
-            // pathing for assets
-            bigDaddy.SpriteIconPath =
-            @"assets\img\Npcs\Hostiles\BigDaddy\BigDaddy_SpriteIcon.png";
-            bigDaddy.InteractionIconPath =
-            @"assets\img\Npcs\Hostiles\BigDaddy\BigDaddy_InteractionIcon.png";
-            bigDaddy.DeathSoundPath =
-            @"assets\sounds\npcs\Hostiles\BigDaddy\BigDaddy_EnemyDeath.wav";
-            bigDaddy.CriticalHitSoundPath =
-            @"assets\sounds\npcs\Hostiles\BigDaddy\BigDaddy_EnemyCriticalHit.wav";
+            Enemy bigDaddy = MapEntityFactory.CreateEnemy(
+                "BigDaddy",
+                200,
+                7.5,
+                30,
+                false,
+                @"assets\img\Npcs\Hostiles\BigDaddy\BigDaddy_SpriteIcon.png",
+                @"assets\img\Npcs\Hostiles\BigDaddy\BigDaddy_InteractionIcon.png",
+                @"assets\sounds\npcs\Hostiles\BigDaddy\BigDaddy_EnemyDeath.wav",
+                @"assets\sounds\npcs\Hostiles\BigDaddy\BigDaddy_EnemyCriticalHit.wav");
 
             // [BOSS] Nemesis
-            Enemy nemesis = new Enemy("Nemesis", 500, 25, 50, true);
-            // pathing for assets
-            nemesis.SpriteIconPath =
-            @"assets\img\Npcs\Hostiles\Nemesis\Nemesis_SpriteIcon.png";
-            nemesis.InteractionIconPath =
-            @"assets\img\Npcs\Hostiles\Nemesis\Nemesis_InteractionIcon.png";
-            nemesis.DeathSoundPath =
-            @"assets\sounds\npcs\Hostiles\Nemesis\Nemesis_EnemyDeath.wav";
-            nemesis.CriticalHitSoundPath =
-            @"assets\sounds\npcs\Hostiles\Nemesis\Nemesis_EnemyCriticalHit.wav";
+            Enemy nemesis = MapEntityFactory.CreateEnemy(
+                "Nemesis",
+                500,
+                25,
+                50,
+                true,
+                @"assets\img\Npcs\Hostiles\Nemesis\Nemesis_SpriteIcon.png",
+                @"assets\img\Npcs\Hostiles\Nemesis\Nemesis_InteractionIcon.png",
+                @"assets\sounds\npcs\Hostiles\Nemesis\Nemesis_EnemyDeath.wav",
+                @"assets\sounds\npcs\Hostiles\Nemesis\Nemesis_EnemyCriticalHit.wav");
 
             // add enemies to map
             map1.MapHostiles.Add(regenerator);
@@ -78,31 +68,24 @@ namespace Group_Project {
             map1.MapHostiles.Add(nemesis);
 
             // [NPC] FireKeeper
-            FriendlyNPC fireKeeper = new FriendlyNPC("FireKeeper");
-            // pathing for assets
-            fireKeeper.SpriteIconPath =
-            @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_SpriteIcon.png";
-            fireKeeper.InteractionIconPath =
-            @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_InteractionIcon.png";
-            fireKeeper.RequiredItem = null;
+            FriendlyNPC fireKeeper = MapEntityFactory.CreateNpc(
+                "FireKeeper",
+                @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_SpriteIcon.png",
+                @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_InteractionIcon.png");
 
             // [NPC] Siegward
-            FriendlyNPC siegward = new FriendlyNPC("Siegward");
-            // pathing for assets
-            siegward.SpriteIconPath =
-            @"assets\img\Npcs\RecruitableNpcs\Siegward\Siegward_SpriteIcon.jpg";
-            siegward.InteractionIconPath =
-            @"assets\img\Npcs\RecruitableNpcs\Siegward\Siegward_InteractionIcon.jpg";
-            siegward.RequiredItem = "Siegbrau";
+            FriendlyNPC siegward = MapEntityFactory.CreateNpc(
+                "Siegward",
+                @"assets\img\Npcs\RecruitableNpcs\Siegward\Siegward_SpriteIcon.jpg",
+                @"assets\img\Npcs\RecruitableNpcs\Siegward\Siegward_InteractionIcon.jpg",
+                "Siegbrau");
 
             // [NPC] Solaire
-            FriendlyNPC solaire = new FriendlyNPC("Solaire");
-            // pathing for assets
-            solaire.SpriteIconPath =
-            @"assets\img\Npcs\RecruitableNpcs\Solaire\Solaire_SpriteIcon.png";
-            solaire.InteractionIconPath =
-            @"assets\img\Npcs\RecruitableNpcs\Solaire\Solaire_InteractionIcon.png";
-            solaire.RequiredItem = "Sunlight Medal";
+            FriendlyNPC solaire = MapEntityFactory.CreateNpc(
+                "Solaire",
+                @"assets\img\Npcs\RecruitableNpcs\Solaire\Solaire_SpriteIcon.png",
+                @"assets\img\Npcs\RecruitableNpcs\Solaire\Solaire_InteractionIcon.png",
+                "Sunlight Medal");
 
             // add NPCs to map
             map1.MapNPCs.Add(fireKeeper);
@@ -110,20 +93,19 @@ namespace Group_Project {
             map1.MapNPCs.Add(solaire);
 
             // instantiate + add items to map
-            Item firstAidSpray = new Item
-            ("First Aid Spray",
-            "Restores Health",
-            "A single-use item that fully restores the player's health when used in combat.",
-            @"assets\img\MapItems\assets_img_MapItems_FirstAidSpray.png",
-            2);
+            Item firstAidSpray = MapEntityFactory.CreateItem(
+                "First Aid Spray",
+                "Restores Health",
+                "A single-use item that fully restores the player's health when used in combat.",
+                @"assets\img\MapItems\assets_img_MapItems_FirstAidSpray.png",
+                2);
 
-            Item siegbrau = new Item
-            ("Siegbrau",
-            "A warm drink for Siegward.",
-            "Give to Siegward to recruit him.",
-            "PLACEHOLDER PATHING",
-            1
-            );
+            Item siegbrau = MapEntityFactory.CreateItem(
+                "Siegbrau",
+                "A warm drink for Siegward.",
+                "Give to Siegward to recruit him.",
+                "PLACEHOLDER PATHING",
+                1);
 
             map1.MapItems.Add(firstAidSpray);
             map1.MapItems.Add(siegbrau);
@@ -153,28 +135,28 @@ namespace Group_Project {
              */
 
             // [ITEM] First Aid Spray
-            m1_Section1.Tiles.Add(40, MakeTile(5, 5, TileType.Item, "First Aid Spray",
-                @"assets\img\MapItems\assets_img_MapItems_FirstAidSpray.png"));
+            MapEntityFactory.AddTile(m1_Section1, 5, 5, TileType.Item, "First Aid Spray",
+                @"assets\img\MapItems\assets_img_MapItems_FirstAidSpray.png");
 
             // [NPC] FireKeeper
-            m1_Section1.Tiles.Add(26, MakeTile(3, 5, TileType.LevelUp, "FireKeeper",
-                @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_SpriteIcon.png"));
+            MapEntityFactory.AddTile(m1_Section1, 3, 5, TileType.LevelUp, "FireKeeper",
+                @"assets\img\Npcs\NPCs\FireKeeper\FireKeeper_SpriteIcon.png");
 
             // [ENEMY] Dr. Salvador
-            m1_Section1.Tiles.Add(9, MakeTile(1, 2, TileType.Enemy, "DrSalvador",
-                @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_SpriteIcon.jpg"));
+            MapEntityFactory.AddTile(m1_Section1, 1, 2, TileType.Enemy, "DrSalvador",
+                @"assets\img\Npcs\Hostiles\DrSalvador\DrSalvador_SpriteIcon.jpg");
 
             // [ENEMY] Regenerator
-            m1_Section1.Tiles.Add(29, MakeTile(4, 1, TileType.Enemy, "Regenerator",
-                @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_SpriteIcon.png"));
+            MapEntityFactory.AddTile(m1_Section1, 4, 1, TileType.Enemy, "Regenerator",
+                @"assets\img\Npcs\Hostiles\Regenerator\Regenerator_SpriteIcon.png");
 
             // [INVENTORY]
-            m1_Section1.Tiles.Add(24, MakeTile(3, 3, TileType.Inventory, "Inventory",
-                "PLACEHOLDER"));
+            MapEntityFactory.AddTile(m1_Section1, 3, 3, TileType.Inventory, "Inventory",
+                "PLACEHOLDER");
 
             // [ARROW] leads to Section 2
-            m1_Section1.Tiles.Add(21, MakeTile(3, 0, TileType.ArrowLeft,
-                imagePath: "PLACEHOLDER", destination: 1)); // index of LeftSection
+            MapEntityFactory.AddTile(m1_Section1, 3, 0, TileType.ArrowLeft,
+                imagePath: "PLACEHOLDER", destination: 1); // index of LeftSection
 
 
 
