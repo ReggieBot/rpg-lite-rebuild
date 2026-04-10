ff13230e7f637ff2a243613a21ad050276b4b669
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2026-04-09 22:30:04 -0400

    Add: Created MapEntityFactory with helpers
    Created MapEntityFactory class that contains helper methods which can be
    called by other MapFactory classes to assist in map scaffolding
    Hoping this cuts down on the already mounting LOC by reducing redundancy
    in object instantiation
    
    The helper methods I created are:
        CreateTile()
        AddTile()
        CreateEnemy()
        CreateNPC()
        CreateItem()
    
    All methods use the `internal` access modifier, and can be accessed from
    anywhere in the same project assembly.

---
+++ src/Group Project/Maps/MapEntityFactory.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Factory class containing helper methods to create map entities
+// Sections, tiles, enemies, NPCs, and items
+// These methods are then called in the MapFactory classes
+// Created to avoid cluttering the MapFactory classes with repeated code for creating entities
+
+namespace Group_Project {
+    internal static class MapEntityFactory
+    {
+
+        // Create new tile instance with the specified properties
+        internal static Tile CreateTile(int row, int col, TileType type, string entityName = null,
+            string imagePath = null, int destination = -1)
+        {
+            Tile tile = new Tile(row, col, type);
+            tile.EntityName = entityName;
+            tile.ImagePath = imagePath;
+            tile.DestinationSectionIndex = destination;
+            return tile;
+        }
+
+        // Add tiles to section
+        internal static void AddTile(Section section, int row, int col, TileType type, string entityName = null,
+            string imagePath = null, int destination = -1)
+        {
+            int index = row * 7 + col;
+            Tile tile = MakeTile(row, col, type, entityName, imagePath, destination);
+            section.Tiles.Add(index, tile);
+        }
+
+        // Create Enemy instance
+        internal static Enemy CreateEnemy(
+            string name,
+            double maxHealth,
+            double damage,
+            float xpReward,
+            bool isBoss,
+            string spritePath,
+            string interactionPath,
+            string deathSoundPath,
+            string criticalHitSoundPath)
+        {
+            Enemy enemy = new Enemy(name, maxHealth, damage, xpReward, isBoss);
+            enemy.SpriteIconPath = spritePath;
+            enemy.InteractionIconPath = interactionPath;
+            enemy.DeathSoundPath = deathSoundPath;
+            enemy.CriticalHitSoundPath = criticalHitSoundPath;
+            return enemy;
+        }
+
+        // Create FriendlyNPC instance
+        internal static FriendlyNPC CreateNpc(
+            string name,
+            string spritePath,
+            string interactionPath,
+            string requiredItem = null)
+        {
+            FriendlyNPC npc = new FriendlyNPC(name);
+            npc.SpriteIconPath = spritePath;
+            npc.InteractionIconPath = interactionPath;
+            npc.RequiredItem = requiredItem;
+            return npc;
+        }
+
+        // create Item instance
+        internal static Item CreateItem(
+            string name,
+            string description,
+            string usageInfo,
+            string imagePath,
+            byte quantity = 1)
+        {
+            Item item = new Item(name, description, usageInfo, imagePath, quantity);
+            return item;
+        }
+    }
+}
