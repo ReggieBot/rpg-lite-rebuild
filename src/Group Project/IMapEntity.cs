b837081f79fae4fd95f37b854605d895fc6e717e
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2026-04-17 03:15:18 -0400

    Implemented interface for map entities
    Interface created with the intention of replacing the brittle
    string-match lookup in the FindX() functions. This way, a tile can hold
    a direct reference to the entity on top of it, instead of relying on
    'entityName'
    
    Enemy, FriendlyNPC, and Item classes now need to implement this

---
+++ src/Group Project/IMapEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+Interface for map entities.
+Any entity that is placed on a tile must implement this interface
+This design allows for polymorphism when placing entities on tiles, as all
+entities will have a name property that can be accessed regardless of
+their specific type ( Enemy, NPC, Item, etc.)
+
+This was made with the intention of replacing the string-match lookup for the FindX() functions
+This way, a tile can hold a direct reference to the entity it contains.
+
+ */
+
+
+namespace Group_Project
+{
+    public interface IMapEntity
+    {
+        string name { get; }
+    }
+}
