42da48c57a4db83733fdb203eb938799291e4ca8
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2026-04-20 07:16:04 -0400

    Started fleshing out GameSessionFactory
    Created mehtod for InitializeALlMaps() and a small amount of save logic.
    
    Save restoration still isn't built though, so this will probably
    change (depending on scope).

--- src/Group Project/GameSessionFactory.cs
+++ src/Group Project/GameSessionFactory.cs
@@ -6,7 +6,25 @@ namespace Group_Project
     {
         public static GameSession CreateNewSession()
         {
+            // save/load setup
+            GameStateManager saveManager = new GameStateManager();
 
+            // map construction
+            List<GameMap> maps = InitializeAllMaps();
+            SaveData saveData = saveManager.LoadGame();
+
+
+        }
+
+        private static List<GameMap> InitializeAllMaps()
+        {
+            List<GameMap> maps = new List<GameMap>();
+
+            // build first map
+            GameMap map1 = Map1Factory.BuildMap1();
+            maps.Add(map1);
+
+            return maps;
         }
     }
 }
