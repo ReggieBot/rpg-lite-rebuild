b4d70cb0a91d858cb00d54ae1674942f85c532c9
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2026-04-20 07:04:28 -0400

    Moving final logic out of Form1 with GameSessionFactory.cs
    Form1 still owns the creation of maps, loads save data, chose player/map
    index, and created the session.
    
    That's still too much logic for a layer that is supposed to be UI.
    
    Created a GameSessionFactory class that returns a fully instantiated
    GameSession.
    
    That GameSession contains everything we should need (I hope...)

---
+++ src/Group Project/GameSessionFactory.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Group_Project
+{
+    public static class GameSessionFactory
+    {
+        public static GameSession CreateNewSession()
+        {
+
+        }
+    }
+}
